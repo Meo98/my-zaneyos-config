e9272827a8f89c7f574487fc5140b817f5bb14f0
Don Williams <don.e.williams@gmail.com>
2026-01-05 17:49:02 -0500

    Removed dup entry

--- modules/home/hyprland/exec-once.nix
+++ modules/home/hyprland/exec-once.nix
@@ -38,7 +38,6 @@ in {
         "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user start hyprpolkitagent"
         "qs -c overview" # Start quickshell-overview daemon
-        "noctalia-shell &"
       ]
       ++ noctaliaExec ++ waybarExec;
   };
