790ecddd876a80f11e32a6533b090bd088e276a0
Meo98 <chenevard.romeo@gmail.com>
2026-01-28 21:06:17 +0100

    fix: Remove deprecated gestures options for newer Hyprland

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -87,15 +87,8 @@ in {
         };
       };
 
-      gestures = {
-        workspace_swipe = true;
-        workspace_swipe_distance = 500;
-        workspace_swipe_invert = true;
-        workspace_swipe_min_speed_to_force = 30;
-        workspace_swipe_cancel_ratio = 0.5;
-        workspace_swipe_create_new = true;
-        workspace_swipe_forever = true;
-      };
+      # Gestures für Touchpad (in neueren Hyprland-Versionen anders konfiguriert)
+      # gestures werden jetzt über input.touchpad gesteuert
 
       misc = {
         layers_hog_keyboard_focus = true;
